78dea1b2086b60426cd5d09a4f7ab9ed7d4dea3b
Jon Norman <jonte.n@gmail.com>
2022-02-07 12:19:30 +0100

    Updated cloud tools

--- home-manager/modules/cloud.nix
+++ home-manager/modules/cloud.nix
@@ -2,12 +2,19 @@
 {
   # Install misc cloud, IaC and virtualisation tools without configuration
   home.packages = with pkgs; [
+    cloud-custodian
     google-cloud-sdk
+    kubernetes-helm
+    kubecolor
     kubectl
     kubectx
+    kubepug
     k9s
     minikube
     packer
+    podman
+    podman-compose
+    popeye
     terraform
     terraform-providers.google
     terraform-providers.github
