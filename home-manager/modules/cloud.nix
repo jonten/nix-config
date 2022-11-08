0e23aa5c83b20fc14409f5be089669c45356d086
Jon Norman <jonte.n@gmail.com>
2022-11-09 00:01:38 +0100

    Added more k8s related tools

--- home-manager/modules/cloud.nix
+++ home-manager/modules/cloud.nix
@@ -3,13 +3,16 @@
   # Install misc cloud, IaC and virtualisation tools without configuration
   home.packages = with pkgs; [
     google-cloud-sdk
+    k3s
+    k9s
+    kind
     kpt
     kubecolor
     kubectl
     #kubectx
     #kubeprompt
     kubernetes-helm
-    k9s
+    kustomize
     minikube
     packer
     terraform
